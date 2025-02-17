8edfcf71f382c73dce4797abca3757e8701e699c
xjy <xjy@123.com>
2025-02-17 12:28:23 +0800

    去掉不必要库引用

--- app/src/main/java/com/example/newtcpapp/MainActivity.java
+++ app/src/main/java/com/example/newtcpapp/MainActivity.java
@@ -1,31 +1,10 @@
 package com.example.newtcpapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.TextView;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.util.Log;
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.Closeable;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
-import android.content.pm.PackageManager;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements TCPServer.Callback {
     private TextView serverInfoTextView;
@@ -49,14 +28,18 @@ public class MainActivity extends AppCompatActivity implements TCPServer.Callbac
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        tcpServer.stopServer();
+        if (tcpServer != null) {
+            tcpServer.stopServer();
+        }
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onServerStarted(String ip, int port) {
         serverInfoTextView.setText("服务器信息：" + ip + ":" + port);
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onClientConnected(String clientIp) {
         connectionStatusTextView.setText("客户端连接: " + clientIp);
@@ -72,6 +55,7 @@ public class MainActivity extends AppCompatActivity implements TCPServer.Callbac
 
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onError(String errorMessage) {
         connectionStatusTextView.setText("服务器错误: " + errorMessage);
